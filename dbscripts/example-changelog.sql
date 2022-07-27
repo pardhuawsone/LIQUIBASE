a085be804cfa896afe7335789e38061a236960b1
pardhuawsone <pardhuawsone@gmail.com>
2022-07-27 15:19:26 +0530

    Update example-changelog.sql

--- dbscripts/example-changelog.sql
+++ dbscripts/example-changelog.sql
@@ -1,8 +1,8 @@
 --liquibase formatted sql
 
---changeset saradhi.makkena:4 labels:example-label context:example-context
+--changeset saradhi.makkena:8 labels:example-label context:example-context
 --comment: example comment
-create table demo.student (
+create table demo1.student (
     name varchar(50),
     address1 varchar(50),
     address2 varchar(50),
@@ -10,9 +10,9 @@ create table demo.student (
 )
 --rollback DROP TABLE person;
 
---changeset saradhi.makkena:5 labels:example-label context:example-context
+--changeset saradhi.makkena:9 labels:example-label context:example-context
 --comment: example comment
-create table demo.employee123 (
+create table demo1.employee123 (
     name varchar(50),
     address1 varchar(50),
     address2 varchar(50),
@@ -20,14 +20,14 @@ create table demo.employee123 (
 )
 --rollback DROP TABLE company;
 
---changeset other.dev:6 labels:example-label context:example-context
+--changeset other.dev:10 labels:example-label context:example-context
 --comment: example comment
 
-alter table demo.employee123 add column country varchar(2)
+alter table demo1.employee123 add column country varchar(2)
 --rollback ALTER TABLE person DROP COLUMN country;
 
---changeset saradhi.makkena:7
-create table demo.TREE (
+--changeset saradhi.makkena:11
+create table demo1.TREE (
     name varchar(50),
     leafs varchar(50),
     height number(22)
